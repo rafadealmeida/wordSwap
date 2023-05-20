3ad8768920c53d6482d8eefd5c80ed9e79dfa727
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2023-05-20 09:56:14 -0300

    [wordSwap - 1] Configs exibidor de pdf

--- src/app/teste/page.tsx
+++ src/app/teste/page.tsx
@@ -3,9 +3,12 @@
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import { Document, Page, pdfjs } from 'react-pdf';
 import { IconButton, Stack } from '@mui/material';
 import AttachFileIcon from '@mui/icons-material/AttachFile';
 import { useState } from 'react';
+
+pdfjs.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.16.105/pdf.worker.min.js';
 
 const bull = (
   <Box
@@ -17,13 +20,13 @@ const bull = (
 );
 
 export default function BasicCard() {
-  const [image, setImage] = useState<string>();
+  const [file, setFile] = useState<string>('');
 
   const handleFile = (event: any): void => {
     const file = event.target?.files[0];
-    const fileURL = URL.createObjectURL(file);
-    console.log(typeof fileURL);
-    setImage(fileURL);
+    // const fileURL = URL.createObjectURL(file);
+    // console.log(typeof fileURL);
+    setFile(file);
   };
 
   return (
@@ -34,14 +37,16 @@ export default function BasicCard() {
         <input hidden type="file" id="file" onChange={handleFile} />
       </IconButton>
       <Box sx={{ width: '40%', backgroundColor: '#fff', height: '25vh' }}>
-        {image ? (
+        {file ? (
           // eslint-disable-next-line @next/next/no-img-element
-          <img
-            src={image}
-            alt="image"
-            width={'100%'}
-            height={'100%'}
-          />
+          // <img src={file} alt="file" width={'100%'} height={'100%'} />
+          <Document file={file}>
+            <Page
+              pageNumber={1}
+              renderAnnotationLayer={false}
+              renderTextLayer={false}
+            />
+          </Document>
         ) : (
           <Typography color="black" component={'span'}>
             Selecione uma imagem
